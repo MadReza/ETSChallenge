2ca9950562f7f550df9e64dcf9838b80433c25bc
Alex Tse <alexnovtse@gmail.com>
2015-02-07 11:23:20 -0500

    fixes to emergency contacts activity

--- APP/app/src/main/java/scs/resistors/firstresponder/EmergencyContacts.java
+++ APP/app/src/main/java/scs/resistors/firstresponder/EmergencyContacts.java
@@ -1,6 +1,7 @@
 package scs.resistors.firstresponder;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,16 +21,69 @@ import android.widget.Toast;
 public class EmergencyContacts extends ActionBarActivity {
 
     private static final int CONTACT_PICKER_RESULT = 1001;
-    private int currentIndex = 1;
+    private int currentIndex = 0;
     private int itemsDeleted = 3;
     private int itemsAdded = 0;
+    private String[] nameArray = new String[3];
+    private String[] phoneArray = new String[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_contacts);
+
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        TextView output1 = (TextView) findViewById(R.id.txt_output1);
+        TextView output2 = (TextView) findViewById(R.id.txt_output2);
+        TextView output3 = (TextView) findViewById(R.id.txt_output3);
+
+        if (settings.contains("currentIndex")) {
+            currentIndex = settings.getInt("currentIndex", currentIndex);
+        }
+
+        if (settings.contains("itemsAdded")) {
+            itemsAdded = settings.getInt("itemsAdded", itemsAdded);
+        }
+
+        if (settings.contains("itemsDeleted")) {
+            itemsDeleted = settings.getInt("itemsDeleted", itemsDeleted);
+        }
+
+        if (settings.contains("contact1name") && settings.contains("contact1phone")) {
+            output1.setText(settings.getString("contact1name", "Name") +
+                    "          Phone #: " +
+                    settings.getString("contact1phone", ""));
+            nameArray[0] = settings.getString("contact1name", "Name");
+            phoneArray[0] = settings.getString("contact1phone", "");
+        }
+        if (settings.contains("contact2name") && settings.contains("contact2phone")) {
+            output2.setText(settings.getString("contact2name", "Name") +
+                    "          Phone #: " +
+                    settings.getString("contact2phone", ""));
+            nameArray[1] = settings.getString("contact2name", "Name");
+            phoneArray[1] = settings.getString("contact2phone", "");
+        }
+        if (settings.contains("contact3name") && settings.contains("contact3phone")) {
+            output3.setText(settings.getString("contact3name", "Name") +
+                    "          Phone #: " +
+                    settings.getString("contact3phone", ""));
+            nameArray[2] = settings.getString("contact3name", "Name");
+            phoneArray[2] = settings.getString("contact3phone", "");
+        }
+
         Button deleteButton = (Button) findViewById(R.id.btn_delete_contact);
-        deleteButton.setVisibility(View.INVISIBLE);
+        if (itemsDeleted == 3)
+            deleteButton.setVisibility(View.INVISIBLE);
+        else
+            deleteButton.setVisibility(View.VISIBLE);
+
+        Button addButton = (Button) findViewById(R.id.btn_add_contacts);
+        if (itemsAdded == 3) {
+            addButton.setVisibility(View.INVISIBLE);
+        }
+        else
+            addButton.setVisibility(View.VISIBLE);
+
     }
 
 
@@ -74,14 +128,14 @@ public class EmergencyContacts extends ActionBarActivity {
                         String id = result.getLastPathSegment();
 
                         String whereName = ContactsContract.Data.MIMETYPE + " = ? AND " + ContactsContract.CommonDataKinds.StructuredName.CONTACT_ID + " = ?";
-                        String[] whereNameParams = new String[] { ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, id };
+                        String[] whereNameParams = new String[]{ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, id};
 
                         cursor = getContentResolver().query(ContactsContract.Data.CONTENT_URI,
                                 null, whereName, whereNameParams,
                                 ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
 
                         phoneCursor = getContentResolver().query(Phone.CONTENT_URI,
-                                null, Phone.CONTACT_ID + "=?", new String[] { id },
+                                null, Phone.CONTACT_ID + "=?", new String[]{id},
                                 null);
 
                         int nameIdx = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
@@ -114,26 +168,32 @@ public class EmergencyContacts extends ActionBarActivity {
                         TextView output = (TextView) findViewById(R.id.txt_output1);
                         itemsDeleted--;
                         switch (currentIndex) {
-                            case 1:
+                            case 0:
                                 output = (TextView) findViewById(R.id.txt_output1);
                                 currentIndex++;
                                 itemsAdded++;
                                 break;
-                            case 2:
+                            case 1:
                                 output = (TextView) findViewById(R.id.txt_output2);
                                 currentIndex++;
                                 itemsAdded++;
                                 break;
-                            case 3:
+                            case 2:
                                 output = (TextView) findViewById(R.id.txt_output3);
-                                currentIndex = 1;
+                                currentIndex = 0;
                                 itemsAdded++;
                                 break;
                             default:
-                                currentIndex = 1;
                                 break;
                         }
                         output.setText(name + "          Phone #: " + phone);
+                        int pastIndex = currentIndex - 1;
+                        if (currentIndex == 0) {
+                            pastIndex = 2;
+                        }
+                        nameArray[pastIndex] = name;
+                        phoneArray[pastIndex] = phone;
+
                         if (itemsAdded == 3) {
                             Button addButton = (Button) findViewById(R.id.btn_add_contacts);
                             addButton.setVisibility(View.INVISIBLE);
@@ -163,22 +223,22 @@ public class EmergencyContacts extends ActionBarActivity {
         startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
     }
 
-    public void deleteContact (View view) {
+    public void deleteContact(View view) {
 
         itemsAdded--;
         TextView output = (TextView) findViewById(R.id.txt_output1);
-        if (currentIndex == 1)
-            currentIndex = 3;
+        if (currentIndex == 0)
+            currentIndex = 2;
         else
             currentIndex--;
         switch (currentIndex) {
-            case 1:
+            case 0:
                 output = (TextView) findViewById(R.id.txt_output1);
                 break;
-            case 2:
+            case 1:
                 output = (TextView) findViewById(R.id.txt_output2);
                 break;
-            case 3:
+            case 2:
                 output = (TextView) findViewById(R.id.txt_output3);
                 break;
             default:
@@ -192,6 +252,8 @@ public class EmergencyContacts extends ActionBarActivity {
 
         if (output.getText() != "None") {
             output.setText("None");
+            nameArray[currentIndex] = null;
+            phoneArray[currentIndex] = null;
             itemsDeleted++;
         }
 
@@ -203,10 +265,51 @@ public class EmergencyContacts extends ActionBarActivity {
         Button deleteButton = (Button) findViewById(R.id.btn_delete_contact);
         if (itemsDeleted == 3) {
             deleteButton.setVisibility(View.INVISIBLE);
-        }
-        else
+        } else
             deleteButton.setVisibility(View.VISIBLE);
 
     }
+
+    public void done(View view){
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+
+        if (nameArray[0] != "None") {
+            editor.putString("contact1name", nameArray[0]);
+            editor.putString("contact1phone", phoneArray[0]);
+        }
+        else {
+            editor.putString("contact1name", null);
+            editor.putString("contact1phone", null);
+        }
+
+        if (nameArray[1] != "None") {
+            editor.putString("contact2name", nameArray[1]);
+            editor.putString("contact2phone", phoneArray[1]);
+        }
+        else {
+            editor.putString("contact2name", null);
+            editor.putString("contact2phone", null);
+        }
+
+        if (nameArray[2] != "None") {
+            editor.putString("contact3name", nameArray[2]);
+            editor.putString("contact3phone", phoneArray[2]);
+        }
+        else {
+            editor.putString("contact3name", null);
+            editor.putString("contact3phone", null);
+        }
+
+        editor.putInt("currentIndex", currentIndex);
+        editor.putInt("itemsAdded", itemsAdded);
+        editor.putInt("itemsDeleted", itemsDeleted);
+
+        editor.apply();
+
+        Toast.makeText(this, "Contacts Saved.",
+                Toast.LENGTH_LONG).show();
+    }
+
 
 }
