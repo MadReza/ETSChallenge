212ac155993a2b9f817801379f2d96654964bec3
AmineMhedhbi <m.amine.mhedhbi@gmail.com>
2015-02-07 08:46:30 -0500

    fixes yoohoo

--- APP/app/src/main/java/scs/resistors/firstresponder/MainActivity.java
+++ APP/app/src/main/java/scs/resistors/firstresponder/MainActivity.java
@@ -3,14 +3,9 @@ package scs.resistors.firstresponder;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,227 +21,56 @@ public class MainActivity extends ActionBarActivity {
 
         // GET ImageView-elements
         ImageView didYouKnowImageView = (ImageView) findViewById(R.id.didYouKnowImageView);
-        didYouKnowImageView.setDrawingCacheEnabled(true);
         ImageView alarmImageView = (ImageView) findViewById(R.id.firstResponderImageView);
-        alarmImageView.setDrawingCacheEnabled(true);
         ImageView toolBoxImageView = (ImageView) findViewById(R.id.toolBoxImageView);
-        toolBoxImageView.setDrawingCacheEnabled(true);
         ImageView caloriesImageView = (ImageView) findViewById(R.id.caloriesImageViews);
-        caloriesImageView.setDrawingCacheEnabled(true);
         ImageView FAQImageView = (ImageView) findViewById(R.id.faqImageView);
-        FAQImageView.setDrawingCacheEnabled(true);
 
-        // DID YA KNOW ACTIVITY BUTTON
-        didYouKnowImageView.setOnTouchListener(new View.OnTouchListener() {
+        didYouKnowImageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(final View v, MotionEvent event) {
-                Bitmap bmp = Bitmap.createBitmap(v.getDrawingCache());
-                ImageView startRunButtonImageView = (ImageView) v;
-                int color = 0;
-                try {
-                    color = bmp.getPixel((int) event.getX(), (int) event.getY());
-                } catch(Exception e) {
-                    android.util.Log.e(TAG,"getting the Bitmap" +
-                            " Pixel touched for startRunButton threw an exception");
-                }
-                if(color == Color.TRANSPARENT) return false;
-                else {
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_UP:
-                            // load darker image
-                            // startRunButtonImageView.setImageResource(R.drawable.startrun_pressed);
-                            try {
-                                Thread.sleep(1);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            Intent intent = new Intent(getApplicationContext(), DidYouKnowActivity.class);
-                            startActivity(intent);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                return true;
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DidYouKnowActivity.class);
+                startActivity(intent);
             }
         });
-
-        // First Responder ACTIVITY BUTTON
-        alarmImageView.setOnTouchListener(new View.OnTouchListener() {
+        alarmImageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(final View v, MotionEvent event) {
-                Bitmap bmp = Bitmap.createBitmap(v.getDrawingCache());
-                ImageView viewHistoryButtonImageView = (ImageView) v;
-                int color = 0;
-                try {
-                    color = bmp.getPixel((int) event.getX(), (int) event.getY());
-                } catch (Exception e) {
-                    android.util.Log.e(TAG, "getting the Bitmap" +
-                            " Pixel touched for viewHistoryButton threw an exception");
-                }
-                if (color == Color.TRANSPARENT) return false;
-                else {
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_UP:
-                            // load the dark ImageView
-                            //viewHistoryButtonImageView.setImageResource(R.drawable.viewhistory_pressed);
-                            try {
-                                Thread.sleep(1);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            Intent intent = new Intent(getApplicationContext(), FirstResponder.class);
-                            startActivity(intent);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                return true;
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FirstResponder.class);
+                startActivity(intent);
             }
         });
-
-        // First Responder ACTIVITY BUTTON
-        toolBoxImageView.setOnTouchListener(new View.OnTouchListener() {
+        toolBoxImageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(final View v, MotionEvent event) {
-                Bitmap bmp = Bitmap.createBitmap(v.getDrawingCache());
-                ImageView viewHistoryButtonImageView = (ImageView) v;
-                int color = 0;
-                try {
-                    color = bmp.getPixel((int) event.getX(), (int) event.getY());
-                } catch (Exception e) {
-                    android.util.Log.e(TAG, "getting the Bitmap" +
-                            " Pixel touched for viewHistoryButton threw an exception");
-                }
-                if (color == Color.TRANSPARENT) return false;
-                else {
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_UP:
-                            AlertDialog.Builder alert = new AlertDialog.Builder(
-                                    MainActivity.this);
-                            alert.setTitle("About");
-                            alert.setMessage("Health Toolbox.\nVersion 0.0.1");
-                            alert.setNegativeButton("Okay", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-                            AlertDialog dialog = alert.show();
-                            TextView messageView = (TextView) dialog.findViewById(android.R.id.message);
-                            messageView.setGravity(Gravity.CENTER);
-                            break;
-                        default:
-                            break;
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(
+                        MainActivity.this);
+                alert.setTitle("About");
+                alert.setMessage("Health Toolbox.\nVersion 0.0.1");
+                alert.setNegativeButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
                     }
-                }
-                return true;
+                });
+                AlertDialog dialog = alert.show();
+                TextView messageView = (TextView) dialog.findViewById(android.R.id.message);
+                messageView.setGravity(Gravity.CENTER);
             }
         });
-
-        // First Responder ACTIVITY BUTTON
-     /*   alarmImageView.setOnTouchListener(new View.OnTouchListener() {
+        caloriesImageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(final View v, MotionEvent event) {
-                Bitmap bmp = Bitmap.createBitmap(v.getDrawingCache());
-                ImageView viewHistoryButtonImageView = (ImageView) v;
-                int color = 0;
-                try {
-                    color = bmp.getPixel((int) event.getX(), (int) event.getY());
-                } catch (Exception e) {
-                    android.util.Log.e(TAG, "getting the Bitmap" +
-                            " Pixel touched for viewHistoryButton threw an exception");
-                }
-                if (color == Color.TRANSPARENT) return false;
-                else {
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_UP:
-                            // load the dark ImageView
-                            //viewHistoryButtonImageView.setImageResource(R.drawable.viewhistory_pressed);
-                            try {
-                                Thread.sleep(1);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            Intent intent = new Intent(getApplicationContext(), FirstResponder.class);
-                            startActivity(intent);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                return true;
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CalorieCounterActivity.class);
+                startActivity(intent);
             }
         });
-
-        // First Responder ACTIVITY BUTTON
-        alarmImageView.setOnTouchListener(new View.OnTouchListener() {
+        FAQImageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(final View v, MotionEvent event) {
-                Bitmap bmp = Bitmap.createBitmap(v.getDrawingCache());
-                ImageView viewHistoryButtonImageView = (ImageView) v;
-                int color = 0;
-                try {
-                    color = bmp.getPixel((int) event.getX(), (int) event.getY());
-                } catch (Exception e) {
-                    android.util.Log.e(TAG, "getting the Bitmap" +
-                            " Pixel touched for viewHistoryButton threw an exception");
-                }
-                if (color == Color.TRANSPARENT) return false;
-                else {
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_UP:
-                            // load the dark ImageView
-                            //viewHistoryButtonImageView.setImageResource(R.drawable.viewhistory_pressed);
-                            try {
-                                Thread.sleep(1);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            Intent intent = new Intent(getApplicationContext(), FirstResponder.class);
-                            startActivity(intent);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                return true;
+            public void onClick(View view) {
+                //Intent intent = new Intent(getApplicationContext(), FAQActivity.class);
+                //startActivity(intent);
             }
-        });*/
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-
-        } else if (id == R.id.action_personalize_me) {
-
-        } else if (id == R.id.action_fun_facts) {
-            Intent intent = new Intent (this, DidYouKnowActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.action_settings) {
-
-        } else if (id == R.id.action_faq) {
-
-        }else if(id == R.id.action_calorie){
-            Intent intent = new Intent (this, CalorieCounterActivity.class);
-            startActivity(intent);
-        }
-
-        return super.onOptionsItemSelected(item);
+        });
     }
 }
