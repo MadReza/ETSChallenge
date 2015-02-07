4f59350b10691319524816deae7ce9f3cb8c83c4
AmineMhedhbi <m.amine.mhedhbi@gmail.com>
2015-02-07 12:14:55 -0500

    about to test SmsManager

--- APP/app/src/main/java/scs/resistors/firstresponder/FirstResponder.java
+++ APP/app/src/main/java/scs/resistors/firstresponder/FirstResponder.java
@@ -1,6 +1,7 @@
 package scs.resistors.firstresponder;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.telephony.SmsManager;
 
 public class FirstResponder extends ActionBarActivity {
 
@@ -54,6 +56,7 @@ public class FirstResponder extends ActionBarActivity {
                 personalizeButton.setClickable(false);
 
                 // Trigger sound shit
+
                 new CountDownTimer(30000, 1000) {
                     public void onTick(long millisUntilFinished) {
                         cancelTextView.setText
@@ -63,7 +66,15 @@ public class FirstResponder extends ActionBarActivity {
                     }
                     public void onFinish() {
                         // WORK HAPPESNS HERE
+                        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+                        String phoneNo1 = prefs.getString("contact1phone", null);
+                        String phoneNo2 = prefs.getString("contact2phone", null);
+                        String phoneNo3 = prefs.getString("contact3phone", null);
 
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(phoneNo1, null, getResources().getString(R.string.SmsMessage), null, null);
+                        smsManager.sendTextMessage(phoneNo2, null, getResources().getString(R.string.SmsMessage), null, null);
+                        smsManager.sendTextMessage(phoneNo3, null, getResources().getString(R.string.SmsMessage), null, null);
 
                         // end up going to displayActivity
                         Intent intent = new Intent (getApplicationContext(),
