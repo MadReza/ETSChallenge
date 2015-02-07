95d5b93d7996696de17eba7820faef0e8de25e95
AmineMhedhbi <m.amine.mhedhbi@gmail.com>
2015-02-07 10:30:23 -0500

    init work

--- APP/app/src/main/java/scs/resistors/firstresponder/FirstResponder.java
+++ APP/app/src/main/java/scs/resistors/firstresponder/FirstResponder.java
@@ -2,9 +2,11 @@ package scs.resistors.firstresponder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class FirstResponder extends ActionBarActivity {
 
@@ -21,12 +23,31 @@ public class FirstResponder extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+
         Button erContactsButton = (Button) findViewById(R.id.erContactsButton);
         erContactsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (getApplicationContext(), EmergencyContacts.class);
                 startActivity(intent);
+            }
+        });
+
+        ImageView panicImageView = (ImageView) findViewById(R.id.panic);
+        panicImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new CountDownTimer(30000, 1000) {
+                    public void onTick(long millisUntilFinished) {
+                        mTextField.setText
+                                ("seconds remaining before action starts: "
+                                        + millisUntilFinished / 1000);
+                    }
+                    public void onFinish() {
+                        mTextField.setText("done!");
+                    }
+                }.start();
+
             }
         });
     }
