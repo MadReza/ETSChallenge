dfba4d7ab94b2edad355f5660c76b3384f2c842d
AmineMhedhbi <m.amine.mhedhbi@gmail.com>
2015-02-07 00:24:43 -0500

    fixes maybe?

--- APP/app/src/main/java/scs/resistors/firstresponder/MainActivity.java
+++ APP/app/src/main/java/scs/resistors/firstresponder/MainActivity.java
@@ -1,13 +1,12 @@
 package scs.resistors.firstresponder;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
