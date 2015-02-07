43bf11e028f7da31ffaa204eec317a27b6641799
AmineMhedhbi <m.amine.mhedhbi@gmail.com>
2015-02-07 07:10:53 -0500

    finalizations

--- APP/app/src/main/java/scs/resistors/firstresponder/DidYouKnowActivity.java
+++ APP/app/src/main/java/scs/resistors/firstresponder/DidYouKnowActivity.java
@@ -1,13 +1,7 @@
 package scs.resistors.firstresponder;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -42,61 +36,7 @@ public class DidYouKnowActivity extends ActionBarActivity {
         };
         showFactButton.setOnClickListener(listener);
 
-        SharedPreferences sharedpreferences = getPreferences(MODE_WORLD_READABLE);
-        Editor editor = sharedpreferences.edit();
-        editor.putInt(this.getString(R.string.wiki_pref), 0); // By default not registered
-        editor.commit();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_didyaknow, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.wiki_register) {
-
-            AlertDialog.Builder alert = new AlertDialog.Builder(
-                    DidYouKnowActivity.this);
-            alert.setTitle("Healthy Tips");
-            SharedPreferences sharedpreferences = getPreferences(MODE_WORLD_READABLE);
-            if (0 == sharedpreferences.getInt(getResources().getString(R.string.prefs), 0)) {
-                alert.setMessage("Would you like to get daily healthy tips as a notification? Just register");
-                alert.setPositiveButton("REGISTER", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        SharedPreferences sharedpreferences = getPreferences(MODE_WORLD_READABLE);
-                        Editor editor = getPreferences(MODE_WORLD_READABLE).edit();
-                        editor.putInt(getResources().getString(R.string.wiki_pref), 1);
-                        editor.commit();
-                    }
-                });
-            } else if (1 == sharedpreferences.getInt(getResources().getString(R.string.prefs), 1)) {
-                alert.setMessage("You are registered to get daily healthy tips! Wanna unregister?");
-                alert.setPositiveButton("UNREGISTER", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        SharedPreferences sharedpreferences = getPreferences(MODE_WORLD_READABLE);
-                        Editor editor = getPreferences(MODE_WORLD_READABLE).edit();
-                        editor.putInt(getResources().getString(R.string.wiki_pref), 0);
-                        editor.commit();
-                    }
-                });
-            }
-            alert.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            });
-            alert.show();
-        }
-
-        return super.onOptionsItemSelected(item);
+        if(null != getActionBar())
+            getActionBar().hide();
     }
 }
